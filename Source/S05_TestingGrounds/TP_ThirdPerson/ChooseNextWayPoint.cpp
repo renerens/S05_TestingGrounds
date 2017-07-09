9a80fab54eca667ed434923cfc3e4a0ab5440861
RENEGADE-PC\Renegade <rene.rens@gmail.com>
2017-07-09 20:06:43 +0200

    Reading Blackboard Data from C++

--- Source/S05_TestingGrounds/TP_ThirdPerson/ChooseNextWayPoint.cpp
+++ Source/S05_TestingGrounds/TP_ThirdPerson/ChooseNextWayPoint.cpp
@@ -1,10 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWayPoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Executed Task!"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"),Index);
 
 	return EBTNodeResult::Succeeded;
 }
